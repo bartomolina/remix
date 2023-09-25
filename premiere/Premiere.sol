0bb12833fc2f07357de69abf262332a411df4053
Barto Molina <bartomolina@gmail.com>
2023-09-25 11:57:27 +0200

    updated

---
+++ premiere/Premiere.sol
@@ -0,0 +1,113 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import {IConstantFlowAgreementV1} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/agreements/IConstantFlowAgreementV1.sol";
+import {ISuperfluidToken} from "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import {IRegistry} from "./IRegistry.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+
+// Superfluid CFA
+// Polygon: 0x6EeE6060f715257b970700bc2656De21dEdF074C
+// Mumbai: 0x49e565Ed1bdc17F3d220f72DF0857C26FA83F873
+
+// SuperToken (fDAIx / DAIx)
+// Polygon: 0x1305F6B6Df9Dc47159D12Eb7aC2804d4A33173c2
+// Mumbai: 0x5D8B4C2554aeB7e86F387B4d6c00Ac33499Ed01f
+
+// LensHub
+// Polygon: 0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d
+// Mumbai: 0x60Ae865ee4C725cd04353b5AAb364553f56ceF82
+
+// FlowRate
+// 2 / mo: 761035007610
+// 5 / mo: 1902587519025
+// 10 / mo: 3805175038051
+
+contract Premiere {
+    address private _cfaAddress = 0x49e565Ed1bdc17F3d220f72DF0857C26FA83F873;
+    address private _superTokenAddress = 0x5D8B4C2554aeB7e86F387B4d6c00Ac33499Ed01f;
+    address private _lensHubAddress = 0x60Ae865ee4C725cd04353b5AAb364553f56ceF82;
+    address private _tbaRegistry = 0x02101dfB77FDE026414827Fdc604ddAF224F0921;
+    string private _minFlowRate = "761035007610";
+    string private _maxTimestamp = "";
+
+    function isSubscribed(
+        address sender,
+        address receiver,
+        string memory profileId,
+        string memory minFlowRate,
+        string memory maxTimestamp
+    ) public view returns (bool subscribed) {
+        subscribed = false;
+
+        IERC721 _lensHub = IERC721(_lensHubAddress);
+        IConstantFlowAgreementV1 _cfa = IConstantFlowAgreementV1(
+            _cfaAddress
+        );
+        (uint256 timestamp, int96 flowRate, , ) = _cfa.getFlow(
+            ISuperfluidToken(_superTokenAddress),
+            sender,
+            receiver
+        );
+        address _owner = _lensHub.ownerOf(uint(parseInt(profileId)));
+
+        int256 _flowRate = int(parseInt(minFlowRate));
+        uint256 _timestamp = parseInt(maxTimestamp);
+
+        if (
+            (flowRate >= _flowRate &&
+                (_timestamp == 0 || timestamp < _timestamp)) ||
+            sender == _owner
+        ) {
+            subscribed = true;
+        }
+    }
+
+    function balanceOf(address _owner, uint256 _id) external view returns (uint256) {
+        address _tba = IRegistry(_tbaRegistry).account(_lensHubAddress, _id);
+        if (isSubscribed(_owner, _tba, Strings.toString(_id), _minFlowRate, _maxTimestamp)) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    function parseInt(
+        string memory _a
+    ) internal pure returns (uint256 _parsedInt) {
+        return parseInt(_a, 0);
+    }
+
+    function parseInt(
+        string memory _a,
+        uint _b
+    ) internal pure returns (uint256 _parsedInt) {
+        bytes memory bresult = bytes(_a);
+        uint mint = 0;
+        bool decimals = false;
+        for (uint i = 0; i < bresult.length; i++) {
+            if (
+                (uint(uint8(bresult[i])) >= 48) &&
+                (uint(uint8(bresult[i])) <= 57)
+            ) {
+                if (decimals) {
+                    if (_b == 0) {
+                        break;
+                    } else {
+                        _b--;
+                    }
+                }
+                mint *= 10;
+                mint += uint(uint8(bresult[i])) - 48;
+            } else if (uint(uint8(bresult[i])) == 46) {
+                decimals = true;
+            }
+        }
+        if (_b > 0) {
+            mint *= 10 ** _b;
+        }
+        return mint;
+    }
+}
